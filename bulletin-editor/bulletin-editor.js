86188f58319aa6cf1dd87ce6ef71e5efb99d168b
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-10-04 10:54:39 -0700

    fetch sending form data to supabase

--- bulletin-editor/bulletin-editor.js
+++ bulletin-editor/bulletin-editor.js
@@ -1,1 +1,41 @@
 import '../auth/user.js';
+import { createBulletin } from '../fetch-utils.js';
+
+const bulletinForm = document.getElementById('bulletin-form');
+const errorDisplay = document.getElementById('error-display');
+
+let error = null;
+
+// Events
+
+bulletinForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(bulletinForm);
+
+    const bulletin = {
+        title: formData.get('title'),
+        description: formData.get('description'),
+        category: formData.get('category'),
+        contact: formData.get('contact'),
+    };
+
+    const response = await createBulletin(bulletin);
+    error = response.error;
+
+    if (error) {
+        displayError();
+    } else {
+        location.assign('/');
+    }
+});
+
+//  Display functions
+
+function displayError() {
+    if (error) {
+        errorDisplay.textContent = error.message;
+    } else {
+        errorDisplay.textContent = '';
+    }
+}
