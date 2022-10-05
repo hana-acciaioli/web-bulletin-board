52d4ebcdbe0e05ecc56885a648453c6e1aace8c9
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-10-04 22:33:47 -0700

    images fetching

--- bulletin-editor/bulletin-editor.js
+++ bulletin-editor/bulletin-editor.js
@@ -1,23 +1,40 @@
 import '../auth/user.js';
-import { createBulletin } from '../fetch-utils.js';
+import { createBulletin, uploadImage } from '../fetch-utils.js';
 
 const bulletinForm = document.getElementById('bulletin-form');
 const errorDisplay = document.getElementById('error-display');
+const imageInput = document.getElementById('image-input');
+const preview = document.getElementById('preview');
 
 let error = null;
 
 // Events
 
+imageInput.addEventListener('change', () => {
+    const file = imageInput.files[0];
+    if (file) {
+        preview.src = URL.createObjectURL(file);
+    } else {
+        preview.src = '../assets/preview.png';
+    }
+});
+
 bulletinForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const formData = new FormData(bulletinForm);
+    const imageFile = formData.get('image');
+    const randomFolder = Math.floor(Date.now() * Math.random());
+    const imagePath = `bulletin_board/${randomFolder}/${imageFile.name}`;
+
+    const url = await uploadImage('images', imagePath, imageFile);
 
     const bulletin = {
         title: formData.get('title'),
         description: formData.get('description'),
         category: formData.get('category'),
         contact: formData.get('contact'),
+        image_url: url,
     };
 
     const response = await createBulletin(bulletin);
--- fetch-utils.js
+++ fetch-utils.js
@@ -44,3 +44,20 @@ export async function getCategories() {
 export async function getBulletins() {
     return await client.from('bulletin_board').select('*').order('created_at').limit(50);
 }
+
+export async function uploadImage(bucketName, imagePath, imageFile) {
+    const bucket = client.storage.from(bucketName);
+    const response = await bucket.upload(imagePath, imageFile, {
+        cacheControl: '3600',
+        upsert: true,
+    });
+
+    if (response.error) {
+        console.log(response.error);
+        return null;
+    }
+
+    const url = `${SUPABASE_URL}/storage/v1/object/public/${response.data.Key}`;
+
+    return url;
+}
